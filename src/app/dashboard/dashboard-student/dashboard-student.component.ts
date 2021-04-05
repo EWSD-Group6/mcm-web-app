67bdaa82f9103d86c1adff7365c1e21297faaf18
Duc <66027624+lightspect@users.noreply.github.com>
2021-04-05 21:04:10 +0700

    feat: login background student dashboard

--- src/app/dashboard/dashboard-student/dashboard-student.component.ts
+++ src/app/dashboard/dashboard-student/dashboard-student.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {SessionService} from '../../contribute-session/state/session.service';
+import {SessionQuery} from '../../contribute-session/state/session.query';
+import {Session} from '../../contribute-session/state/session.model';
+import { NzCalendarMode } from 'ng-zorro-antd/calendar';
+
 
 @Component({
   selector: 'app-dashboard-student',
@@ -7,7 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardStudentComponent implements OnInit {
 
-  constructor() { }
+  deadline: number;
+  date = new Date(2012, 11, 21);
+  mode: NzCalendarMode = 'month';
+
+  panelChange(change: { date: Date; mode: string }): void {
+    console.log(change.date, change.mode);
+  }
+
+  constructor(private service:SessionService) {
+    this.deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
+  }
 
   ngOnInit(): void {
   }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import {RouterModule} from '@angular/router';
 import {NgxChartsModule} from "@swimlane/ngx-charts";
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { DashboardStudentComponent } from './dashboard-student/dashboard-student.component';
 import { DashboardMarketingCoordinatorComponent } from './dashboard-marketing-coordinator/dashboard-marketing-coordinator.component';
 import { DashboardGuestComponent } from './dashboard-guest/dashboard-guest.component';
 import { DashboardMarketingManagerComponent } from './dashboard-marketing-manager/dashboard-marketing-manager.component';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
+import {NzNotificationModule} from 'ng-zorro-antd/notification';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzSpaceModule} from 'ng-zorro-antd/space';
+import {NzCardModule} from 'ng-zorro-antd/card';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [DashboardComponent, DashboardStudentComponent, DashboardMarketingCoordinatorComponent, DashboardGuestComponent, DashboardMarketingManagerComponent, DashboardAdminComponent],
@@ -17,6 +27,14 @@ import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.compo
       {path: '', pathMatch: 'full', component: DashboardComponent}
     ]),
     NgxChartsModule,
+    NzStatisticModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCardModule,
+    NzSpaceModule,
+    NzCalendarModule,
+    ReactiveFormsModule,
   ]
 })
 export class DashboardModule { }
