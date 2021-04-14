4af6d07185adeab8f026d704b789f3ae6d767d06
Duc <66027624+lightspect@users.noreply.github.com>
2021-04-14 12:25:16 +0700

    feat: replace app icon and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
+  public constructor(private titleService: Title) { }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+  ngOnInit(): void {
+    this.setTitle("Magazine Collab Web");
+  }
 }
